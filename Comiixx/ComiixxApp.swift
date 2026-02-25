1f5b6fc2c2689eb6b7f10d6ba49f59e19f26ed7a
Pierluigi De Meo <pierludm2002@gmail.com>
2026-02-25 17:12:49 +0100

    Initial Commit

---
+++ Comiixx/ComiixxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComiixxApp.swift
+//  Comiixx
+//
+//  Created by Pierluigi De Meo on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ComiixxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComiixxTests/ComiixxTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComiixxTests.swift
+//  ComiixxTests
+//
+//  Created by Pierluigi De Meo on 25/02/26.
+//
+
+import Testing
+@testable import Comiixx
+
+struct ComiixxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
